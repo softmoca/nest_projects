5bdb6ca0a379eeb0529ec81fba75e939e44168d8
softmoca <acyc0909@gmail.com>
2023-03-03 16:37:09 +0900

    'swagger_cors'

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -4,32 +4,45 @@ import { UseFilters, UseInterceptors } from '@nestjs/common/decorators';
 import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
 import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
 import { CatRequestDto } from './dto/cats.request.dto';
+import { ApiOperation } from '@nestjs/swagger/dist';
+import { ApiResponse } from '@nestjs/swagger/dist/decorators/api-response.decorator';
+import { ReadOnlyCatDto } from './dto/cat.dto';
 
 @Controller('cats')
 @UseInterceptors(SuccessInterceptor)
 @UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
+  @ApiOperation({ summary: '현재 고양이 가져오기' })
   @Get() //현재 로그인을 한 고양이
   getCurrentCat() {
     return 'current cat';
   }
-
+  @ApiResponse({
+    status: 500,
+    description: 'Server Error...',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '성공!',
+    type: ReadOnlyCatDto,
+  })
+  @ApiOperation({ summary: '회원가입' })
   @Post() //회원가입
   async signUp(@Body() body: CatRequestDto) {
     return await this.catsService.signUp(body);
   }
-
+  @ApiOperation({ summary: '로그인' })
   @Post('login') //로그인
   logIn() {
     return 'login';
   }
-
+  @ApiOperation({ summary: '로그아웃' })
   @Post('logout') //로그아웃
   logOut() {
     return 'logout';
   }
-
+  @ApiOperation({ summary: '고양이 이미지 업로드' })
   @Post('upload/cats') //사진 올리기
   uploadCatImg() {
     return 'uploadImg';
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 //
@@ -7,6 +9,19 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('C.I.C')
+    .setDescription('cat')
+    .setVersion('1.0.0')
+    .build();
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document); //스웨어페이지의 엔드포인트
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
   const PORT = process.env.PORT;
   await app.listen(PORT);
 }
